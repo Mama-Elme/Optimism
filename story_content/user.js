cc9fb3e294d2aecd823e3b559714c6f8dd023e70
Mama-Elme <elme.snyman@gmail.com>
2024-01-02 10:52:32 +0200

    Optimism

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6XqHwU6HkKs":
+        Script1();
+        break;
+      case "68igaG0cnoC":
+        Script2();
+        break;
+      case "5lxXpoO5rA5":
+        Script3();
+        break;
+      case "6SX9ABJCqzz":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
+function Script4()
+{
+  window.print();
+}
+
